d39a1c09af8eaddc763d94c390ece80de147455b
Yury Fyodorov <yuri.inpost@yandex.ru>
2019-12-20 15:33:45 +0300

    content(src): starting rebuild MainOffer

--- src/components/MainOffer/MainOffer.tsx
+++ src/components/MainOffer/MainOffer.tsx
@@ -6,13 +6,13 @@ import iphone6s from '../../img/iPhone6s_discover_desktop_08_iOS9.svg';
 
 const MainOffer: React.FC = () => {
   return (
-    <section className="main-offer">
-        <div className="offer-description">
+    <section className="offer">
+        <div className="offer__description">
             <div className="offer-title">iPhone 6 Plus</div>
             <div className="offer-text">Perfomance and design. Taken right to the edge.</div>
             <a href={'/shop-now'} className="offer-link">SHOP NOW</a>
         </div>
-        <img src={iphone6s} alt="iPhone6s" className="offer-image" />
+        <img src={iphone6s} alt="iPhone6s" className="offer__image" />
     </section>
   )
 }
