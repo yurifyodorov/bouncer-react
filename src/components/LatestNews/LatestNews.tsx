31f5854d468b642df7cdbeeb8f0b3ea2e7818905
Yury Fyodorov <yuri.inpost@yandex.ru>
2019-12-24 14:06:54 +0300

    content(src): edit

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -15,24 +15,24 @@ const LatestNews: React.FC = () => {
                 <img src={news1} alt="news1" className="news1" />
                 <div className="news-description">
                     <div className="news-date">01 Jan, 2015</div>
-                    <h3 className="news-title">Typesetting industry</h3>
-                    <p className="news-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <h3>Typesetting industry</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 </div>
             </div>
             <div className="news-link">
                 <img src={news2} alt="news2" className="news2" />
                 <div className="news-description">
                     <div className="news-date">01 Jan, 2015</div>
-                    <h3 className="news-title">Typesetting industry</h3>
-                    <p className="news-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <h3>Typesetting industry</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 </div>
             </div>
             <div className="news-link">
                 <img src={news3} alt="news3" className="news3" />
                 <div className="news-description">
                     <div className="news-date">01 Jan, 2015</div>
-                    <h3 className="news-title">Typesetting industry</h3>
-                    <p className="news-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <h3>Typesetting industry</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 </div>
             </div>
         </div>
